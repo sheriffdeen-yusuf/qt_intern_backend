d9cbc370023123e3239975ac49dbc974fcf28ff1
Sheriffdeen Yusuf <sheriffdeenyusuf1130@gmail.com>
2022-08-11 23:20:22 +0100

    task 1

---
+++ project_one/myidentity.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var email string = "https://github.com/sheriffdeen-Yusuf"
+	var name string = "Sheriffdeen Yusuf"
+
+	fmt.Println(email)
+	fmt.Println("\n")
+	fmt.Println(name)
+}
---
+++ project_one/mystack.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("my stack is backend \n")
+}
